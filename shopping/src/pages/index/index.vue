8a24a97b962735c774f6b821f42323ed4dc69ab5
sunliang0928 <sunliang0928@163.com>
2019-07-08 17:16:36 +0800

    页面完成

--- shopping/src/pages/index/index.vue
+++ shopping/src/pages/index/index.vue
@@ -4,13 +4,47 @@
 
         </header>
         <section class="main">
-
-        </section>
+            <div class="sl_list">
+               <div class="sl_item">
+                    <div class="sl_title"><li><span class="sl_title_span">精选好物</span><span>等你来抢</span></li><p>更多</p></div>
+                    <BottomList />
+                    <div class="sl_imgBox">
+                        <img src="https://jnup.oss-cn-beijing.aliyuncs.com/product/218b4c27298b0b1ceb771217bd7017b9.jpg" alt="">
+                    </div>
+               </div>
+               <div class="sl_item">
+                    <div class="sl_title"><li><span class="sl_title_span">精选好物</span><span>等你来抢</span></li><p>更多</p></div>
+                    <BottomList />
+                    <div class="sl_imgBox">
+                        <img src="https://jnup.oss-cn-beijing.aliyuncs.com/product/218b4c27298b0b1ceb771217bd7017b9.jpg" alt="">
+                    </div>
+               </div>
+               <div class="sl_item">
+                    <div class="sl_title"><li><span class="sl_title_span">精选好物</span><span>等你来抢</span></li><p>更多</p></div>
+                    <BottomList />
+                    <div class="sl_imgBox">
+                        <img src="https://jnup.oss-cn-beijing.aliyuncs.com/product/218b4c27298b0b1ceb771217bd7017b9.jpg" alt="">
+                    </div>
+               </div>
+               <div class="sl_item">
+                    <div class="sl_title"><li><span class="sl_title_span">精选好物</span><span>等你来抢</span></li><p>更多</p></div>
+                    <BottomList />
+                    <div class="sl_imgBox">
+                        <img src="https://jnup.oss-cn-beijing.aliyuncs.com/product/218b4c27298b0b1ceb771217bd7017b9.jpg" alt="">
+                    </div>
+               </div>
+               <div class="sl_item">
+                    <div class="sl_title"><li><span class="sl_title_span">为你精选</span><span>等你来抢</span></li><p>更多</p></div>
+                    <Item />
+               </div>
+            </div>
+        </section>  
     </div>
 </template>
 
 <script>
-
+import Item from '@/components/sl_bottomItem' 
+import BottomList from '@/components/sl_item'
 export default {
     data () {
         return {
@@ -19,7 +53,8 @@ export default {
     },
 
     components: {
-        
+        BottomList,
+        Item
     },
 
     methods: {
@@ -33,5 +68,51 @@ export default {
 </script>
 
 <style scoped>
-
+.wrap{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+}
+.main{
+    width: 100%;
+    flex: 1;
+}
+.sl_list{
+    width: 100%;
+    height: auto;
+}
+.sl_title{
+    box-sizing: border-box;
+    width: 100%;
+    height: 100rpx;
+    display: flex;
+    padding: 0 15rpx;
+    align-items: center;
+    justify-content: space-between;
+}
+.sl_title li span{
+    font-size: 30rpx;
+    padding-left: 20rpx;
+}
+.sl_title li .sl_title_span{
+    border-right: #ccc solid 1rpx;
+    font-size: 40rpx;
+    padding-right: 20rpx;
+}
+.sl_title p{
+    font-size: 35rpx;
+    color: #fc5d7b;
+} 
+.sl_imgBox{
+    width: 100%;
+    height: 180rpx;
+    box-sizing: border-box;
+    padding: 0 15rpx;
+}
+.sl_imgBox >img{
+    width:100%;
+    height: 100%;
+    border-radius: 10px;
+}
 </style>
---
+++ shopping/src/store/index.js
@@ -0,0 +1,11 @@
+import vue from 'vue'
+import vuex from 'vuex'
+import index from './modules/index'
+vue.use(vuex)
+
+let store = new vuex.Store({
+    modules:{
+        index:index
+    }
+})
+export default store
