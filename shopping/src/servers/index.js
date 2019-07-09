3f5d8ea9095cd1dc62407ad9f47851686c160ef1
a5354159 <390354259@qq.com>
2019-07-09 20:01:13 +0800

    完善优惠卷

--- shopping/src/servers/index.js
+++ shopping/src/servers/index.js
@@ -1,2 +1,4 @@
-import request from '@/utils/request'
-
+import request from "@/utils/request";
+export let getConponList = params => {
+  request.post("http://test.api.jinaup.com/api/open/user/coupon/userCouponInfoList/1.0.0", params);
+};
--- shopping/src/store/index.js
+++ shopping/src/store/index.js
@@ -1,11 +1,13 @@
-import vue from 'vue'
-import vuex from 'vuex'
-import index from './modules/index'
-vue.use(vuex)
+import vue from "vue";
+import vuex from "vuex";
+import index from "./modules/index";
+import Mycoupon from "./modules/zh_Myconpon";
+vue.use(vuex);
 
 let store = new vuex.Store({
-    modules:{
-        index:index
-    }
-})
-export default store
+  modules: {
+    index: index,
+    Mycoupon: Mycoupon
+  }
+});
+export default store;
