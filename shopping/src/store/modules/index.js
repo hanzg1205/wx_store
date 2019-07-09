bd3b98acd960555b017c36279832f22e81553309
sunliang0928 <sunliang0928@163.com>
2019-07-09 09:15:39 +0800

    修改合并

--- shopping/src/store/modules/index.js
+++ shopping/src/store/modules/index.js
@@ -4,16 +4,12 @@ const state={
     tabList: []
 };
 const actions={
-<<<<<<< HEAD
-    
-=======
     async getTab({commit},payload){
         console.log('payload...',payload)
         let data = await getTab(payload);
         console.log('data...',data);
         commit('getTabList', data.result)
     }
->>>>>>> beeb44cab3dafabc378f5b329c31fd67c0dcfae4
 };
 const mutations={
     updateTabIndex(state,payload){
