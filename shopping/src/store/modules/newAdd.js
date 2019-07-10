e68939166f011cd81c6cbdac62a8ab9e475382b9
a5354159 <390354259@qq.com>
2019-07-10 17:22:07 +0800

    shah

--- shopping/src/store/modules/newAdd.js
+++ shopping/src/store/modules/newAdd.js
@@ -1,16 +1,28 @@
 import { addNewAddress } from "@/servers";
-const state = {};
+const state = {
+  lists: []
+};
 const actions = {
   submit({ commit }, payload) {
     return new Promise(async (resolve, reject) => {
       let data = await addNewAddress(payload);
+      commit("addRess", { lists: data.data.result });
+
       resolve(data);
     });
   }
 };
-
+const mutations = {
+  addRess(state, payload) {
+    console.log(state);
+    for (let key in payload) {
+      return (state[key] = payload[key]);
+    }
+  }
+};
 export default {
   namespaced: true,
   state,
-  actions
+  actions,
+  mutations
 };
