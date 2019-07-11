26a05cf265082ddc8bea411616508c804fe05800
sunliang0928 <sunliang0928@163.com>
2019-07-11 20:05:49 +0800

    首页完成

--- shopping/src/pages/sl_banner/index.vue
+++ shopping/src/pages/sl_banner/index.vue
@@ -2,8 +2,8 @@
     <div class="wrap">
         <div class="header">
             <img class="img" :src="bannerList.specialImg" alt="" :style="bannerList.shareImgHeight<=1600?{height:bannerList.shareImgHeight/2+'rpx'}:{height:bannerList.shareImgHeight/10+'rpx'}">
-            <div class="sl_lists">
-                <p v-for="(item,index) in bannerList.anchors" :key="index" class="one">{{item.anchorName}}</p>
+            <div id="sl_lists">
+                <p v-for="(item,index) in bannerList.anchors" :key="index" :class="flag?'one':'none'">{{item.anchorName}}</p>
             </div>
         </div>
         <div class="main">
@@ -22,6 +22,12 @@
 import Babylist from '@/components/sl_babyList'
 import {mapState,mapActions} from 'vuex'
 export default {
+    data(){
+        return {
+            top:'',
+            flag:true
+        }
+    },
     components:{
         Babylist
     },
@@ -33,17 +39,23 @@ export default {
     created(){
        console.log('1234567890',this.bannerList)
     },
-    
+    mounted(){
+        let that = this ;
+        const query = wx.createSelectorQuery()
+            query.select('.one').boundingClientRect()
+            query.selectViewport().scrollOffset()
+            query.exec(function(res){
+                console.log(res)
+            that.top=res[0].top       // #the-id节点的上边界坐标
+            // res[1].scrollTop // 显示区域的竖直滚动位置
+        })
+    },
     onPageScroll(e){
-        console.log(e)
-        if(this.bannerList.shareImgHeight<=1600){
-            if(e.scrollTop>=this.bannerList.shareImgHeight/10){
-                console.log('吸顶')
-            }
-        }else if(this.bannerList.shareImgHeight>=1600){
-            if(e.scrollTop>=this.bannerList.shareImgHeight/100){
-                console.log('吸顶')
-            }
+        console.log(this.top)
+        if(e.scrollTop>=this.top-50){
+           this.flag=false
+        }else{
+            this.flag=true
         }
     }
 }
@@ -70,11 +82,20 @@ export default {
         background: #fff;
         text-indent: 15rpx;
     }
-    .one{
+    .one , .none{
         line-height: 100rpx;
         text-align: center;
         color:red;
         text-indent: 15rpx;
+        background: #fff;
+    }
+    .none{
+        width: 100%;
+        position: fixed;
+        top:0;
+        left:0;
+        text-align: left;
+        z-index:999
     }
     .main{
         width: 100%;
@@ -101,7 +122,7 @@ export default {
         transform: rotate(45deg);
         position: relative;
     }
-    .sl_lists{
+    #sl_lists{
         display: flex;
         width: 100%;
     }
